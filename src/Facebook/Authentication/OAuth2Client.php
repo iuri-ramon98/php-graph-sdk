d8aa3e3303876f9652394b1bfc7ba325d69bccd8
Iuri Ramon Cervantes Santos <53568799+iuri-ramon98@users.noreply.github.com>
2020-09-26 08:59:55 -0300

    Alteracao projeto facebook

--- src/Facebook/Authentication/OAuth2Client.php
+++ src/Facebook/Authentication/OAuth2Client.php
@@ -2,6 +2,7 @@
 /**
  * Copyright 2017 Facebook, Inc.
  *
+ * fazendo alteração para teste de clone
  * You are hereby granted a non-exclusive, worldwide, royalty-free license to
  * use, copy, modify, and distribute this software in source code or binary
  * form for use in connection with the web services and APIs provided by
